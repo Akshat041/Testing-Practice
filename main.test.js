af18f6b7303002a0de3870574a171b53b7b6e88e
Akshat Singh <akshatofficial041@gmail.com>
2026-02-07 03:45:18 +0530

    Add calculator tests for subtraction, division, and multiplication

--- main.test.js
+++ main.test.js
@@ -11,3 +11,15 @@ test("reverse a string", () => {
 test("calculate sum", () => {
   expect(testFunctions.calculator.add(3, 7)).toBe(10);
 });
+
+test("calculate subtraction", () => {
+  expect(testFunctions.calculator.sub(10, 7)).toBe(3);
+});
+
+test("calculate division", () => {
+  expect(testFunctions.calculator.div(10, 2)).toBe(5);
+});
+
+test("calculate multiplication", () => {
+  expect(testFunctions.calculator.mul(3, 7)).toBe(21);
+});
