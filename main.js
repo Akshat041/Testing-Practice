8cebdc374634e627bc75df60e9bdf9d701e5a23d
Akshat Singh <akshatofficial041@gmail.com>
2026-02-28 08:48:15 +0530

    Add analyzeArray function and helper functions for min and max calculations

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// 1. capitalize function that takes a string and returns it with the first character capitalized
 function capitalize(str) {
   const initial = str[0].toUpperCase();
   const remainingStr = str.slice(1);
@@ -5,10 +6,12 @@ function capitalize(str) {
   return initial.concat(remainingStr);
 }
 
+// 2. A reverseString function that takes a string and returns it reversed.
 function reverseString(str) {
   return str.split("").reverse().join("");
 }
 
+// 3.  calculator object that contains functions for the basic operations.
 const calculator = {
   add: function (a, b) {
     return a + b;
@@ -24,6 +27,7 @@ const calculator = {
   },
 };
 
+// 4. A caesarCipher function that takes a string and a shift factor and returns it with each character “shifted”.
 function caesarCipher(text, key) {
   const alphabet = [
     "a",
@@ -91,9 +95,48 @@ function caesarCipher(text, key) {
   return cipherText;
 }
 
+// 5. An analyzeArray function that takes an array of numbers and returns an object with the following properties: average, min, max, and length
+function analyzeArray(arr) {
+  let average, min, max, length;
+  let sum = 0;
+
+  length = arr.length;
+
+  for (let i = 0; i < length; i++) {
+    sum += arr[i];
+  }
+  average = sum / length;
+
+  min = findMin(arr);
+
+  max = findMax(arr);
+
+  return { average, min, max, length };
+}
+
+function findMin(arr) {
+  let min = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (min > arr[i]) {
+      min = arr[i];
+    }
+  }
+  return min;
+}
+function findMax(arr) {
+  let max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
 module.exports = {
   capitalize,
   reverseString,
   calculator,
   caesarCipher,
+  analyzeArray,
 };
