af964cb8644d74c2e9be98fa9da5df66c8917e0d
Akshat Singh <akshatofficial041@gmail.com>
2026-02-28 08:53:25 +0530

    Remove caesarCipher function and its related code

--- main.js
+++ main.js
@@ -27,74 +27,6 @@ const calculator = {
   },
 };
 
-// 4. A caesarCipher function that takes a string and a shift factor and returns it with each character “shifted”.
-function caesarCipher(text, key) {
-  const alphabet = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
-  const txtArr = [...text];
-  const res = "";
-  let flag = 0;
-
-  const cipherText = txtArr
-    .map((char) => {
-      if (
-        char === "." ||
-        char === "," ||
-        char === " " ||
-        char === "?" ||
-        char === "!" ||
-        char === ":" ||
-        char === ";" ||
-        char === "'" ||
-        char === "-" ||
-        char === "_" ||
-        char === "(" ||
-        char === ")"
-      ) {
-        return res + char;
-      }
-      if (char === char.toUpperCase()) {
-        flag = 1;
-        char = char.toLowerCase();
-      } else {
-        flag = 0;
-      }
-
-      return flag == 1
-        ? res + alphabet[(alphabet.indexOf(char) + key) % 26].toUpperCase()
-        : res + alphabet[(alphabet.indexOf(char) + key) % 26].toLowerCase();
-    })
-    .join("");
-
-  return cipherText;
-}
-
 // 5. An analyzeArray function that takes an array of numbers and returns an object with the following properties: average, min, max, and length
 function analyzeArray(arr) {
   let average, min, max, length;
@@ -137,6 +69,6 @@ module.exports = {
   capitalize,
   reverseString,
   calculator,
-  caesarCipher,
+  // caesarCipher,
   analyzeArray,
 };
